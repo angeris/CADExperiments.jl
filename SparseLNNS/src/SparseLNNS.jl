c56b921003ba64c8976f0304ccf6e92af82294ed
angeris <guille@angeris.net>
2026-01-07 15:06:33 -0500

    Box log footer summary

--- SparseLNNS/src/SparseLNNS.jl
+++ SparseLNNS/src/SparseLNNS.jl
@@ -281,6 +281,7 @@ end
 function log_footer(io, elapsed_s, alloc_bytes)
     @printf(io, "------+-----------+-----------+-----------+----------+----------+-------\n")
     @printf(io, " time: %9.3f s| alloc: %9.2f MiB\n", elapsed_s, alloc_bytes / 1024^2)
+    @printf(io, "------+-----------+-----------+-----------+----------+----------+-------\n")
     return nothing
 end
 
